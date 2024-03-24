ae8cf7638fd902ee8dd7535e9d0828f43e4fae5d
yufrolov <yuriifrolov39@gmail.com>
2024-03-24 23:13:25 +0300

    Spring Boot initialization

---
+++ src/main/java/com/yufrolov/vocabularytrainer/VocabularyTrainerApplication.java
@@ -0,0 +1,13 @@
+package com.yufrolov.vocabularytrainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocabularyTrainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VocabularyTrainerApplication.class, args);
+	}
+
+}
