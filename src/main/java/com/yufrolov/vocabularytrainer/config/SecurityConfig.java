8f5473ff3293efa272f25b15e01b7c7449a65acf
yufrolov <yuriifrolov39@gmail.com>
2024-06-01 14:13:11 +0300

    Refactoring Spring Configuration

--- src/main/java/com/yufrolov/vocabularytrainer/config/SecurityConfig.java
+++ src/main/java/com/yufrolov/vocabularytrainer/config/SecurityConfig.java
@@ -1,20 +1,16 @@
 package com.yufrolov.vocabularytrainer.config;
 
 import com.yufrolov.vocabularytrainer.filter.JwtRequestFilter;
-import com.yufrolov.vocabularytrainer.service.ProfileService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -23,16 +19,11 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableMethodSecurity(securedEnabled = true)
 @Configuration
 public class SecurityConfig {
-    private final ProfileService profileService;
 
     private final JwtRequestFilter jwtRequestFilter;
 
-    private final PasswordEncoder passwordEncoder;
-
-    public SecurityConfig(ProfileService profileService, JwtRequestFilter jwtRequestFilter, PasswordEncoder passwordEncoder) {
-        this.profileService = profileService;
+    public SecurityConfig(JwtRequestFilter jwtRequestFilter) {
         this.jwtRequestFilter = jwtRequestFilter;
-        this.passwordEncoder = passwordEncoder;
     }
 
     @Bean
@@ -53,16 +44,8 @@ public class SecurityConfig {
     }
 
     @Bean
-    public DaoAuthenticationProvider daoAuthenticationProvider() {
-        var daoAuthenticationProvider = new DaoAuthenticationProvider();
-        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder);
-        daoAuthenticationProvider.setUserDetailsService(profileService);
-        return daoAuthenticationProvider;
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
